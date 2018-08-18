ee57d3ad41355f215df371d409bf00f64664293d
UpUpLittleStar <1310342532@qq.com>
2018-08-18 20:56:54 +0800

    hhh

--- src/test/TestE.java
+++ src/test/TestE.java
@@ -4,7 +4,7 @@ public class TestE {
  public static void main(String args[])
  {
 	 double m=max(3.1,4.5,-3.7,-1);
-	 System.out.print(m);
+	 System.out.print("hhh");
  }
  //参数数量可变算法
  public static double max(double... values)
